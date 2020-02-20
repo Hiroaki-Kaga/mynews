19d164f91c9e2ccede3888b89b16b679218d6eff
Hiroaki-Kaga <strawberrytable451@yahoo.co.jp>
2020-02-20 12:10:57 +0000

    laravel kadai11

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -2,15 +2,15 @@
 @extends('layouts.profile')
 
 
-{{-- profile.blade.phpの@yield('title')に'Myプロフィールの新規作成'を埋め込む --}}
-@section('title', 'Myプロフィールの新規作成')
+{{-- profile.blade.phpの@yield('title')に'My プロフィールの新規作成'を埋め込む --}}
+@section('title', 'My プロフィールの新規作成')
 
 {{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>Myプロフィール</h2>
+                <h2>My プロフィール</h2>
             </div>
         </div>
     </div>
