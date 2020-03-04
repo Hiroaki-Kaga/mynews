001e31aaff1389e94f3235cc9c0714098d8cbb22
EC2 Default User <ec2-user@ip-172-31-17-216.us-east-2.compute.internal>
2020-03-04 15:48:44 +0000

    shinki

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -1,31 +1,75 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class ProfileController extends Controller{
-    
-    public function add() {
+// 以下を追記することでNews Modelが扱えるようになる
+use App\Profile;
+
+class ProfileController extends Controller
+{
+
+  public function add()
+  {
       return view('admin.profile.create');
-    }
+ }
+
+ public function create(Request $request)
+ {
+
+
+     // 以下を追記
+     // Varidationを行う
+     $this->validate($request, Profile::$rules);
+
+     $profile = new Profile;
+     $form = $request->all();
+ 
+
+     // フォームから送信されてきた_tokenを削除する
+     unset($form['_token']);
+     
+
+     // データベースに保存する
+     $profile->fill($form);
+     $profile->save();
+
+     return redirect('admin/profile/create');
+ }
+
+
+  public function edit(Request $request)
+  {
+      // News Modelからデータを取得する
+      $profile = Profile::find($request->id);
+      if (empty($profile)) {
+        abort(404);    
+      }
+      return view('admin.profile.edit', ['news_form' => $profile]);
+  }
+      
+  public function update(Request $request)
+  {
+      // Validationをかける
+      $this->validate($request, Profile::$rules);
+      // News Modelからデータを取得する
+      $profile = Profile::find($request->id);
+      // 送信されてきたフォームデータを格納する
+      $profile_form = $request->all();
+     
+      unset($profile_form['_token']);
+      // 該当するデータを上書きして保存する
+      $profile->fill($profile_form)->save();
+
+      return redirect('admin/profile');
+  }
+
+}     
+      
+      
     
-    public function create(Request $request){
 
-      return redirect('admin/profile/create');
-    }
 
-    public function edit(Request $request)
-    {
 
-        return view('admin.profile.edit');
-    }
-    
-    public function update(Request $request){
-    
-        return redirect('admin/profile');
-
-    }
-}
 
